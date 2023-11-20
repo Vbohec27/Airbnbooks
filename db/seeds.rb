3c4d5c5aca2ee6de62dc3bcbe8a1e007a4cad5e6
Vincent Bohec <vbohec@yahoo.fr>
2023-11-20 17:24:51 +0100

    worked on the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Book.create(title: "1984", author: "George Orwell",
+description: "it happens in 1984" )
+Book.create(title: "ND de Paris", author: "Victor Hugo",
+description: "it happens in the 19th century" )
+Book.create(title: "The Stranger", author: "Albert Camus",
+description: "it happens in 20th century" )
+Book.create(title: "American Psycho", author: "Bret Easton Ellis",
+description: "it happens in the eighties" )
+Book.create(title: "Shutter Island", author: "Dennis Lehane",
+description: "it happens on an island" )
