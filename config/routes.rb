afa61e900a6fb78dffee1a6893aa2a23e043105f
Anna Bothe <annabothe0@gmail.com>
2023-11-21 11:35:13 +0100

    made log out button in homepage working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # books and in books there is bookings nested
-  # dashboard a seperate route like books? pages#home? 
+  # dashboard a seperate route like books? pages#home?
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
