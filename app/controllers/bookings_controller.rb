9722ccea3241639e3c475449b4f98eb9c4cc767e
Vincent Bohec <vbohec@yahoo.fr>
2023-11-23 14:56:49 +0100

    updated the new booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,34 +1,45 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show ]
+    before_action :set_book, only: %i[show new create]
 
-  def index
-    @bookings = Booking.all
-  end
+    def index
+      @bookings = Booking.all
+    end
 
-  def show
-  end
+    def show
+    end
 
-  def new
-    @booking = Booking.new
-  end
+    def new
+      @booking = Booking.new
+    end
 
-  def create
-    @booking = Booking.find(booking_params)
-    if @booking.save
-      redirect_to @booking, notice: "Booking was successfully created"
-    else
-      render :new, status: :unprocessable_entity
+    def create
+      @booking = Booking.new(booking_params)
+      @booking.book = @book
+      @booking.user = current_user
+      if @booking.save
+        redirect_to bookings_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def calculate_price(start_date, end_date, price_per_day)
+      days = (end_date - start_date).to_i
+      price_per_day * days
+    end
+
+    def show_total_price
+      @booking = Booking.find(params[booking_params])
+      @total_price = calculate_price(@booking.start_date, @booking.end_date, @booking.book.price_per_day)
+    end
+
+    private
+
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+
+    def booking_params
+      params.require(:booking).permit(:start_date, :end_date)
     end
   end
-
-  private
-
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :books, only: %i[index show new create]
+  resources :books, only: %i[index show new create] do
+      resources :bookings, only:[:new, :create]
+    end
 
 end
