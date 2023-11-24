d7115b13dccc2d01e17c56e9785faf16c2b29ea8
Anna Bothe <annabothe0@gmail.com>
2023-11-24 16:05:10 +0100

    create book and bookings fixed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,6 +37,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :author, :description, :price, :genre, :photo, :address)
+    params.require(:book).permit(:title, :author, :description, :price, :photo, :address)
   end
 end
