0edfa1db09e88ed1c697cfbcfe9d667fc3d1868b
Anna Bothe <annabothe0@gmail.com>
2023-11-24 12:39:37 +0100

    bookings controller indentation fix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,35 +1,35 @@
 class BookingsController < ApplicationController
-    before_action :set_book, only: %i[show new create]
+  before_action :set_book, only: %i[show new create]
 
-    def index
-      @bookings = current_user.bookings
-    end
+  def index
+    @bookings = current_user.bookings
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
-      @booking = Booking.new
-    end
+  def new
+    @booking = Booking.new
+  end
 
-    def create
-      @booking = Booking.new(booking_params)
-      @booking.book = @book
-      @booking.user = current_user
-      if @booking.save
-        redirect_to bookings_path
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    private
-
-    def set_book
-      @book = Book.find(params[:book_id])
-    end
-
-    def booking_params
-      params.require(:booking).permit(:start_date, :end_date, :sum_price)
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.book = @book
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :sum_price)
+  end
+end
